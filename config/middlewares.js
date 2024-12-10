6a64e2006baca9c6aef35657788a5a3496dbbe7b
David Patiño <david.patino1@utp.edu.co>
2024-12-10 09:07:28 -0500

    cloudinary csp fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,13 +1,39 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          // Permite conexión a fuentes seguras y a Cloudinary
+          'connect-src': ["'self'", 'https:'],
+          // Permite cargar imágenes desde 'self', datos, blobs, y tu subdominio de Cloudinary
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // Reemplaza esto si tu subdominio cambia
+          ],
+          // Permite carga de medios desde 'self', datos, blobs y Cloudinary
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // Tu subdominio de Cloudinary
+          ],
+          // Opcional: desactiva las actualizaciones automáticas de solicitudes inseguras
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['*'], // Cambia '*' por los dominios permitidos para mayor seguridad
+      origin: ['*'], // Cambia '*' por tus dominios específicos para mayor seguridad
     },
   },
   'strapi::poweredBy',
